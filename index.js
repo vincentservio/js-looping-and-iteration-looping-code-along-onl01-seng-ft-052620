461a67ee64ea7169853f3444636b76620b7b5214
Vincent Servio <vincentservio@gmail.com>
2020-08-25 22:04:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const name = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+const writeCards = (name) => {
+  const thanks = []
+  for (let i = 0; i < name.length; i++)
+  thanks.push (`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+return thanks
+
+ 
+}
+writeCards(name)
+
+
+const countDown = () =>{
+ let countDown = 0;
+while (countDown < 11) {
+  console.log(countDown++);
+}
+}
