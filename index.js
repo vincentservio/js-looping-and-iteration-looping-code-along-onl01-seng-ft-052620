4d0f595db4df941a5bc11c7ad984190bbe530067
Vincent Servio <vincentservio@gmail.com>
2020-08-25 21:37:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Code your solutions in this fil
-
 const name = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
 const writeCards = (name) => {
   for (let i = 0; i < name.length; i++)
  console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+
+ return name;
  
 }
+writeCards(name)
